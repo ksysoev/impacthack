48352ed21b365805a88525d3dac95c1aa90ccb7a
mahrous-amer <mahrous-amer@protonmail.com>
2023-06-16 19:36:56 +0800

    Somesort of shop api with geo data

--- app/app.js
+++ app/app.js
@@ -1,64 +1,56 @@
-const Redis = require("ioredis");
+const Redis = require('ioredis');
 const express = require('express');
 const bodyParser = require('body-parser');
-const jwt = require('jsonwebtoken');
 
 const app = express();
-const port = process.env.APP_PORT;
-const redis_uri = process.env.REDIS_URL;
-const jwt_secret = process.env.JWT_SECRET || 'KJmCuDI5LaNMcT1cCKVGDg5KFGp6roh5';
-const jwt_expiry = process.env.JWT_EXPIRY || '3600';
-
-const client = new Redis(redis_uri);
-client.on('error', err => console.log('Redis Client Error', err));
+const port = process.env.APP_PORT || 8000;
+const redis_uri = process.env.REDIS_URL || 'redis://localhost:6379';
+const redisClient = new Redis(redis_uri);
 
 app.use(bodyParser.json());
 
-app.post('/api/register', async (req, res) => {
-  try {
-    const { username, password } = req.body;
-    const userId = generateUserId();
-    await client.hmset(`user:${userId}`, 'username', username, 'password', password);
-    res.status(200).json({ message: 'User registered successfully', userId });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
+app.post('/shops', async (req, res) => {
+  const { shopId, shopName, latitude, longitude, products } = req.body;
+  await redisClient.geoadd('shops', longitude, latitude, shopId);
+  await redisClient.hmset(`shop:${shopId}`, 'name', shopName, 'products', JSON.stringify(products));
+  res.status(201).json({ message: 'Shop registered successfully' });
 });
 
-app.post('/api/login', async (req, res) => {
-  try {
-    const { username, password } = req.body;
-    const userId = await getUserId(username);
-    if (userId) {
-      const accessToken = jwt.sign({ userId }, jwt_secret, { expiresIn: jwt_expiry });
-      await client.set(`access_token:${userId}`, accessToken, 'EX', jwt_expiry);
-      res.status(200).json({ accessToken });
+app.get('/search/:productName', async (req, res) => {
+  const { productName } = req.params;
+
+  // Search for shops near the specified product
+  const shopIds = await redisClient.zrangebyscore('shops', '-inf', '+inf');
+  const productPromises = shopIds.map(async (shopId) => {
+    const [shopName, productsJSON] = await redisClient.hmget(`shop:${shopId}`, 'name', 'products');
+    const products = JSON.parse(productsJSON);
+
+    // Exists
+    const matchingProducts = products.filter((product) =>
+      product.toLowerCase().includes(productName.toLowerCase())
+    );
+
+    if (matchingProducts.length > 0) {
+      const geoposResponse = await redisClient.geopos('shops', shopId);
+      const [longitude, latitude] = geoposResponse[0] || []; // undefined
+
+      return {
+        shopId,
+        shopName,
+        location: { latitude: latitude || 0, longitude: longitude || 0 },
+        products: matchingProducts,
+      };
     } else {
-      res.status(401).json({ error: 'Invalid credentials' });
+      return null;
     }
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
+  });
+
+  const results = await Promise.all(productPromises);
+  const matchingShops = results.filter((result) => result !== null);
+
+  res.status(200).json({ matchingShops });
 });
-
-function generateUserId() {
-  return Math.random().toString(36).substring(2, 10);
-}
-
-async function getUserId(username) {
-  const keys = await client.keys('user:*');
-  for (const key of keys) {
-    const user = await client.hgetall(key);
-    if (user.username === username) {
-      const [, userId] = key.split(':');
-      return userId;
-    }
-  }
-  return null;
-}
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Server listening on port ${port}`);
 });
