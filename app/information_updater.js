f4be6aced86e1ff7848f2348cdcb37d8b7b0d054
Кирилл Сысоев <k.sysoev@me.com>
2023-06-17 15:51:23 +0800

    Adds geocoding for updated address information

--- app/information_updater.js
+++ app/information_updater.js
@@ -8,7 +8,7 @@ async function parseResponse(response, currentStructure) {
     
     Response:\n\n${response}
 
-    Structure:\n\n${currentStructure}`;
+    Structure:\n\n${JSON.stringify(currentStructure)}`;
 
     const { data } = await axios.post('https://api.openai.com/v1/chat/completions', {
         messages: [{"role": "user", "content": message}],
@@ -33,9 +33,17 @@ async function parseResponse(response, currentStructure) {
         return;
     }
 
+    if (currentStructure.address !== updatedStructure.address) {
+        const coordinates = await getCoordinates(updatedStructure.address);
+        console.log('coordinates', coordinates);
+        if (coordinates) {
+            updatedStructure.latitude = coordinates.latitude;
+            updatedStructure.longitude = coordinates.longitude;
+        }
+    }
+
     return updatedStructure;
 }
-
 
 async function generataPost(initialMessage, currentStructure) {
     const message = `Parse the following client input and generate social media post from car workshop company. 
@@ -43,7 +51,7 @@ async function generataPost(initialMessage, currentStructure) {
     
     Input:\n\n${initialMessage}
 
-    Structure:\n\n${currentStructure}`;
+    Structure:\n\n${JSON.stringify(currentStructure)}`;
 
     const { data } = await axios.post('https://api.openai.com/v1/chat/completions', {
         messages: [{"role": "user", "content": message}],
@@ -59,18 +67,34 @@ async function generataPost(initialMessage, currentStructure) {
         },
     });
 
-    const post = data.choices[0].message.content;
-    console.log(post);
+    return data.choices[0].message.content;
+}
+
+async function getCoordinates(address) {
+    const url = `https://geocode.maps.co/search?q=${encodeURIComponent(address)}`;
+    console.log('url', url);
+    const { data } = await axios.get(url);
+
+    if (!data.length) {
+        return
+    }
+  
+    const { lat, lng } = data[0].geometry.location;
+  
+    return { latitude: lat, longitude: lng };
 }
 
 
 // Example usage:
 // let updatedStructure = parseResponse(
-//     'yeah, i moved my store to new location, it is now located at 10 jalan sri tasik barat. and now i also working with a new brand for BMW', 
-//     '{"rating":"4","address":"18, Jalan 4/23b, Taman Danau Kota, 53300 Kuala Lumpur, Wilayah Persekutuan Kuala Lumpur","logo":"","latitude":"3.2081691","phone":"+60 16-244 1991","pay_by_card":"true","brands":["Alfa Romeo","Acura","Buick"],"products":{"Seats":"Interior and Exterior Components","Fuel Tank":"Fuel System Components","Sensors (Oxygen Sensor, Mass Air Flow Sensor, etc.)":"Electrical Components","Windows and Windshields":"Interior and Exterior Components","Switches":"Electrical Components","Exterior Trim":"Interior and Exterior Components","Brake Lines":"Braking System Components","Valves":"Engine Components","Brake Discs/Rotors":"Braking System Components","Steering Rack":"Suspension and Steering Components"},"photos":["https://lh5.googleusercontent.com/p/AF1QipOHftDQ8DbpggQ_tOimPJHNXYo0cegRGFC14xta=w800-h500-k-no","https://lh5.googleusercontent.com/p/AF1QipOHftDQ8DbpggQ_tOimPJHNXYo0cegRGFC14xta=w1600-h1000-k-no"],"reliability":"5","category":"Car Wash","longitude":"101.7180254","working_hours":{"Monday":"9:30 am-7:30 pm","Tuesday":"9:30 am-7:30 pm","Wednesday":"9:30 am-7:30 pm","Thursday":"9:30 am-7:30 pm","Friday":"9:30 am-7:30 pm","Saturday":"9:30 am-7:30 pm","Sunday":"9:30 am-7:30 pm"},"shopName":"Danau Air Cond & Accessories Auto Works"}'
+//     'yeah, i moved my store to new location, it is now located at 4, Jln Klang Lama. and now i also working with a new brand for BMW', 
+//     {"rating":"4","address":"18, Jalan 4/23b, Taman Danau Kota, 53300 Kuala Lumpur, Wilayah Persekutuan Kuala Lumpur","logo":"","latitude":"3.2081691","phone":"+60 16-244 1991","pay_by_card":"true","brands":["Alfa Romeo","Acura","Buick"],"products":{"Seats":"Interior and Exterior Components","Fuel Tank":"Fuel System Components","Sensors (Oxygen Sensor, Mass Air Flow Sensor, etc.)":"Electrical Components","Windows and Windshields":"Interior and Exterior Components","Switches":"Electrical Components","Exterior Trim":"Interior and Exterior Components","Brake Lines":"Braking System Components","Valves":"Engine Components","Brake Discs/Rotors":"Braking System Components","Steering Rack":"Suspension and Steering Components"},"photos":["https://lh5.googleusercontent.com/p/AF1QipOHftDQ8DbpggQ_tOimPJHNXYo0cegRGFC14xta=w800-h500-k-no","https://lh5.googleusercontent.com/p/AF1QipOHftDQ8DbpggQ_tOimPJHNXYo0cegRGFC14xta=w1600-h1000-k-no"],"reliability":"5","category":"Car Wash","longitude":"101.7180254","working_hours":{"Monday":"9:30 am-7:30 pm","Tuesday":"9:30 am-7:30 pm","Wednesday":"9:30 am-7:30 pm","Thursday":"9:30 am-7:30 pm","Friday":"9:30 am-7:30 pm","Saturday":"9:30 am-7:30 pm","Sunday":"9:30 am-7:30 pm"},"shopName":"Danau Air Cond & Accessories Auto Works"}
 // );
 
-// let post = generataPost('I have a got new mufler from acrapovich xz123 in stock make it public lah', '{"rating":"4","address":"18, Jalan 4/23b, Taman Danau Kota, 53300 Kuala Lumpur, Wilayah Persekutuan Kuala Lumpur","logo":"","latitude":"3.2081691","phone":"+60 16-244 1991","pay_by_card":"true","brands":["Alfa Romeo","Acura","Buick"],"products":{"Seats":"Interior and Exterior Components","Fuel Tank":"Fuel System Components","Sensors (Oxygen Sensor, Mass Air Flow Sensor, etc.)":"Electrical Components","Windows and Windshields":"Interior and Exterior Components","Switches":"Electrical Components","Exterior Trim":"Interior and Exterior Components","Brake Lines":"Braking System Components","Valves":"Engine Components","Brake Discs/Rotors":"Braking System Components","Steering Rack":"Suspension and Steering Components"},"photos":["https://lh5.googleusercontent.com/p/AF1QipOHftDQ8DbpggQ_tOimPJHNXYo0cegRGFC14xta=w800-h500-k-no","https://lh5.googleusercontent.com/p/AF1QipOHftDQ8DbpggQ_tOimPJHNXYo0cegRGFC14xta=w1600-h1000-k-no"],"reliability":"5","category":"Car Wash","longitude":"101.7180254","working_hours":{"Monday":"9:30 am-7:30 pm","Tuesday":"9:30 am-7:30 pm","Wednesday":"9:30 am-7:30 pm","Thursday":"9:30 am-7:30 pm","Friday":"9:30 am-7:30 pm","Saturday":"9:30 am-7:30 pm","Sunday":"9:30 am-7:30 pm"},"shopName":"Danau Air Cond & Accessories Auto Works"}')
+// let post = generataPost(
+//     'I have a got new mufler from acrapovich xz123 in stock make it public lah', 
+//     {"rating":"4","address":"18, Jalan 4/23b, Taman Danau Kota, 53300 Kuala Lumpur, Wilayah Persekutuan Kuala Lumpur","logo":"","latitude":"3.2081691","phone":"+60 16-244 1991","pay_by_card":"true","brands":["Alfa Romeo","Acura","Buick"],"products":{"Seats":"Interior and Exterior Components","Fuel Tank":"Fuel System Components","Sensors (Oxygen Sensor, Mass Air Flow Sensor, etc.)":"Electrical Components","Windows and Windshields":"Interior and Exterior Components","Switches":"Electrical Components","Exterior Trim":"Interior and Exterior Components","Brake Lines":"Braking System Components","Valves":"Engine Components","Brake Discs/Rotors":"Braking System Components","Steering Rack":"Suspension and Steering Components"},"photos":["https://lh5.googleusercontent.com/p/AF1QipOHftDQ8DbpggQ_tOimPJHNXYo0cegRGFC14xta=w800-h500-k-no","https://lh5.googleusercontent.com/p/AF1QipOHftDQ8DbpggQ_tOimPJHNXYo0cegRGFC14xta=w1600-h1000-k-no"],"reliability":"5","category":"Car Wash","longitude":"101.7180254","working_hours":{"Monday":"9:30 am-7:30 pm","Tuesday":"9:30 am-7:30 pm","Wednesday":"9:30 am-7:30 pm","Thursday":"9:30 am-7:30 pm","Friday":"9:30 am-7:30 pm","Saturday":"9:30 am-7:30 pm","Sunday":"9:30 am-7:30 pm"},"shopName":"Danau Air Cond & Accessories Auto Works"}
+// )
 
 
 module.exports = {
